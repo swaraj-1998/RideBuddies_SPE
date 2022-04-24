f80560746fdf98a3b2b6326a32d934b7a31f5c08
swaraj-1998 <swarajwednesday@gmail.com>
2022-04-24 18:30:20 +0530

    Adding Requestdto

---
+++ src/main/java/riderequestor/RequestDto.java
@@ -0,0 +1,33 @@
+package riderequestor;
+
+public class RequestDto {
+
+	String creater,requester,status;
+
+	
+
+	public String getCreater() {
+		return creater;
+	}
+
+	public void setCreater(String creater) {
+		this.creater = creater;
+	}
+
+	public String getRequester() {
+		return requester;
+	}
+
+	public void setRequester(String requester) {
+		this.requester = requester;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
