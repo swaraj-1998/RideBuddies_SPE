35002b2a07cdc1258ca07362bed9fc37538c63c8
swaraj-1998 <swarajwednesday@gmail.com>
2022-04-24 21:44:32 +0530

    Adding CompleteDTO

---
+++ src/main/java/complete/CompleteDto.java
@@ -0,0 +1,21 @@
+package complete;
+
+public class CompleteDto {
+	String creater,requester;
+
+	public String getCreater() {
+		return creater;
+	}
+
+	public void setCreater(String creater) {
+		this.creater = creater;
+	}
+
+	public String getRequester() {
+		return requester;
+	}
+
+	public void setRequester(String requester) {
+		this.requester = requester;
+	}
+}
