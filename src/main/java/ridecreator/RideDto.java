e88823e5d062ed3217d2f9a3887642dadd873383
swaraj-1998 <swarajwednesday@gmail.com>
2022-04-24 14:56:56 +0530

    Adding RideDto

---
+++ src/main/java/ridecreator/RideDto.java
@@ -0,0 +1,91 @@
+package ridecreator;
+
+public class RideDto {
+
+	String startLoc,endLoc,vehType,mobile,phone;
+	double startLat,endLat,startLng,endLng;
+	int startTime,endTime,people,price;
+	
+	
+	public double getStartLat() {
+		return startLat;
+	}
+	public void setStartLat(double startLat) {
+		this.startLat = startLat;
+	}
+	public double getEndLat() {
+		return endLat;
+	}
+	public void setEndLat(double endLat) {
+		this.endLat = endLat;
+	}
+	public double getStartLng() {
+		return startLng;
+	}
+	public void setStartLng(double startLng) {
+		this.startLng = startLng;
+	}
+	public double getEndLng() {
+		return endLng;
+	}
+	public void setEndLng(double endLng) {
+		this.endLng = endLng;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getStartLoc() {
+		return startLoc;
+	}
+	public void setStartLoc(String startLoc) {
+		this.startLoc = startLoc;
+	}
+	public String getEndLoc() {
+		return endLoc;
+	}
+	public void setEndLoc(String endLoc) {
+		this.endLoc = endLoc;
+	}
+	public String getVehType() {
+		return vehType;
+	}
+	public void setVehType(String vehType) {
+		this.vehType = vehType;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+	public int getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+	public int getPeople() {
+		return people;
+	}
+	public void setPeople(int people) {
+		this.people = people;
+	}
+
+	
+}
